b05a4ddc73d76a5a86d1bde1158967a62aac1d01
yongin <kkc926@naver.com>
2020-10-01 20:54:45 +0900

    first commit

---
+++ button/ex07_2/Button.cpp
@@ -0,0 +1,35 @@
+#include "Button.h"
+
+Button::Button(int pin) : pin(pin){
+    pinMode(pin, INPUT_PULLUP); //하드웨어는 풀업
+    state_previous = true;
+    callback=NULL; //아무것도 지칭안할때 null씀
+}
+
+int Button::read(){
+    return !digitalRead(pin); //소프트웨어는 풀다운으로 운영해야 확장성에 좋음 따라서 !를 붙임
+}
+
+void Button::setCallback(void(*callback)()){
+    this->callback = callback;
+}
+
+int Button::check(){
+    state_current=digitalRead(pin);
+    if(!state_current){ //누른경우
+        if(state_previous == true){
+            state_previous=false;
+            if(callback != NULL){
+                
+                callback();
+            }
+            
+        }
+        
+    
+    }
+    else
+    {
+        state_previous=true;
+    }
+}
---
+++ button/ex07_2/Led.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Arduino.h>
+
+
+
+class Led{ //Digital Device
+    protected:
+    int pin;
+
+public:
+    Led(int pin);
+    void on();
+    void off();
+    void toggle();
+    void set(int value);
+
+};
